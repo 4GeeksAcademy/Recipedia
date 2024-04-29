fae6a40d4da1c89ff6f7b66cc5d9d41ad4ab1ff6
Flavia Ballabene <139667150+FlaviaBallabene@users.noreply.github.com>
2024-04-29 20:27:12 +0000

    first draft chatbot

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,14 +2,14 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { ChatbotSection } from "../component/chatbot/chatbotSection";
+import { ChatBot } from "../component/chatbot/chatbot";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			{/* <h1>Hello Rigo!!</h1>
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
@@ -21,8 +21,8 @@ export const Home = () => {
 				<a href="https://start.4geeksacademy.com/starters/react-flask">
 					Read documentation
 				</a>
-			</p>
-			<ChatbotSection />
+			</p> */}
+			<ChatBot />
 		</div>
 	);
 };
